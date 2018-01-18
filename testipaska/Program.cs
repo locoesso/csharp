f42dcb8cb0242768b793c3b341544e88b8cc6c85
Esa Hanikka <esa.hanikka@gmail.com>
2018-01-18 18:09:59 +0200

    vittu

---
+++ testipaska/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testipaska
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello fuckhead!");
+        }
+    }
+}
